0a651c1f6ba712a17850f6ba133799ad9134ef4a
哈吉米曼波 <1124119304@qq.com>
2026-09-17 00:27:41 +0800

    发表文章

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -177,7 +177,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：广告栏组件 1
 			type: "advertisement",
 			// 是否启用该组件
-			enable: false,
+			enable: true,
 			// 是否显示组件标题
 			showTitle: false,
 			// 组件位置
@@ -208,7 +208,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：广告栏组件 2
 			type: "advertisement",
 			// 是否启用该组件
-			enable: false,
+			enable: true,
 			// 组件位置
 			position: "sticky",
 			// 是否在文章详情页显示
