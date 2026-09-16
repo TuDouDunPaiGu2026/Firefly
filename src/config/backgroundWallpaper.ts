be32a51750e409b075aec6d5dde6b1df1f2999fd
哈吉米曼波 <1124119304@qq.com>
2026-09-16 17:16:36 +0800

    修改文字

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -73,17 +73,17 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否启用主页横幅文字
 			enable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "哈吉米南北绿豆!",
 			// 主页横幅主标题字体大小
 			titleSize: "4.5rem",
 			// 主页横幅副标题
 			subtitle: [
-				"In Reddened Chrysalis, I Once Rest",
-				"From Shattered Sky, I Free Fall",
-				"Amidst Silenced Stars, I Deep Sleep",
-				"Upon Lighted Fyrefly, I Soon Gaze",
-				"From Undreamt Night, I Thence Shine",
-				"In Finalized Morrow, I Full Bloom",
+				"曼波，曼曼波",
+				// "From Shattered Sky, I Free Fall",
+				// "Amidst Silenced Stars, I Deep Sleep",
+				// "Upon Lighted Fyrefly, I Soon Gaze",
+				// "From Undreamt Night, I Thence Shine",
+				// "In Finalized Morrow, I Full Bloom",
 			],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",
